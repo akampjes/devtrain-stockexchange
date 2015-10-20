36155ac5711e33cc8ea445cc4f8a9f9cbf9cd365
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-10-21 10:51:12 +1300

    Test all the order scenarios for basic matching

--- app/services/match_orders.rb
+++ app/services/match_orders.rb
@@ -5,13 +5,14 @@ class MatchOrders
 
     # Basic matching of buy orders against sell orders
     @buy_queue.each do |buy_order|
-      buy_order.update(fulfilled: true)
       @sell_queue.each do |sell_order|
         if !sell_order.fulfilled &&
             match_price?(buy_order, sell_order) &&
             match_quantity?(buy_order, sell_order)
           buy_order.update(fulfilled: true)
           sell_order.update(fulfilled: true)
+          # Skip to the next buy order
+          break
         end
       end
     end
--- spec/services/match_orders_spec.rb
+++ spec/services/match_orders_spec.rb
@@ -6,15 +6,81 @@ RSpec.describe MatchOrders, kind: :service do
 
   context 'a buy order matches against existing sell orders' do
     it 'gets fulfilled' do
-      sell1 =  user.orders.sell.create!(stock: stock, quantity: 100, price: 4)
-      sell2 = user.orders.sell.create!(stock: stock, user: user, kind: 'sell', quantity: 100, price: 1)
-      buy1 = user.orders.buy.create!(stock: stock, user: user, kind: 'buy', quantity: 100, price: 3)
+      sell1 = user.orders.sell.create!(stock: stock, quantity: 100, price: 4)
+      sell2 = user.orders.sell.create!(stock: stock, quantity: 100, price: 1)
+      buy1 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3)
+
+      MatchOrders.new.call
+
+      expect(sell1.reload.fulfilled).to be nil
+      expect(sell2.reload.fulfilled).to be true
+      expect(buy1.reload.fulfilled).to be true
+    end
+  end
+
+  context 'a sell order matches against exsting buy orders' do
+    it 'gets fulfilled' do
+      buy1 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3)
+      buy2 = user.orders.buy.create!(stock: stock, quantity: 100, price: 2)
+      sell1 =  user.orders.sell.create!(stock: stock, quantity: 100, price: 3)
 
       MatchOrders.new.call
 
       expect(buy1.reload.fulfilled).to be true
-      expect(sell2.reload.fulfilled).to be true
+      expect(buy2.reload.fulfilled).to be nil
+      expect(sell1.reload.fulfilled).to be true
+    end
+  end
+
+  context 'a buy order matches against the most oldest matching sell order' do
+    it 'gets fulfilled' do
+      sell1 = user.orders.sell.create!(stock: stock, quantity: 100, price: 3)
+      sell2 = user.orders.sell.create!(stock: stock, quantity: 100, price: 3, created_at: Time.now - 1.day)
+      buy1 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3)
+
+      MatchOrders.new.call
+
       expect(sell1.reload.fulfilled).to be nil
+      expect(sell2.reload.fulfilled).to be true
+      expect(buy1.reload.fulfilled).to be true
+    end
+  end
+
+  context 'a sell order matches against the most oldest matching buy order' do
+    it 'gets fulfilled' do
+      buy1 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3)
+      buy2 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3, created_at: Time.now - 1.day)
+      sell1 = user.orders.sell.create!(stock: stock, quantity: 100, price: 3)
+
+      MatchOrders.new.call
+
+      expect(buy1.reload.fulfilled).to be nil
+      expect(buy2.reload.fulfilled).to be true
+      expect(sell1.reload.fulfilled).to be true
+    end
+  end
+
+  context 'there are no sell orders' do
+    it 'does nothing' do
+      buy1 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3)
+      buy2 = user.orders.buy.create!(stock: stock, quantity: 100, price: 3)
+
+      MatchOrders.new.call
+
+      expect(buy1.reload.fulfilled).to be nil
+      expect(buy2.reload.fulfilled).to be nil
+    end
+  end
+
+  context 'there are no buy orders' do
+    it 'does nothing' do
+      sell1 = user.orders.sell.create!(stock: stock, quantity: 100, price: 3)
+      sell2 = user.orders.sell.create!(stock: stock, quantity: 100, price: 3)
+
+      MatchOrders.new.call
+
+      expect(sell1.reload.fulfilled).to be nil
+      expect(sell2.reload.fulfilled).to be nil
     end
   end
 end
