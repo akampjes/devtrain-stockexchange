72364193bf6d739ff125140ef1657eee4db42480
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-10-21 17:10:28 +1300

    Locking on orders to prevent weirdness

--- app/services/match_orders.rb
+++ app/services/match_orders.rb
@@ -1,18 +1,20 @@
 class MatchOrders
   def call
-    @sell_queue = Order.sell.unfulfilled
-    @buy_queue = Order.buy.unfulfilled
+    Order.transaction do
+      @sell_queue = Order.sell.unfulfilled.lock
+      @buy_queue = Order.buy.unfulfilled.lock
 
-    # Basic matching of buy orders against sell orders
-    @buy_queue.each do |buy_order|
-      @sell_queue.each do |sell_order|
-        if !sell_order.fulfilled &&
-            match_price?(buy_order, sell_order) &&
-            match_quantity?(buy_order, sell_order)
-          buy_order.update(fulfilled: true)
-          sell_order.update(fulfilled: true)
-          # Skip to the next buy order
-          break
+      # Basic matching of buy orders against sell orders
+      @buy_queue.each do |buy_order|
+        @sell_queue.each do |sell_order|
+          if !sell_order.fulfilled &&
+              match_price?(buy_order, sell_order) &&
+              match_quantity?(buy_order, sell_order)
+            buy_order.update(fulfilled: true)
+            sell_order.update(fulfilled: true)
+            # Skip to the next buy order
+            break
+          end
         end
       end
     end
