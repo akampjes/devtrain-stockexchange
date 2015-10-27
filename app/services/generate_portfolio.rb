fc993ded5392f2f2211bc6065a171070435d420f
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-10-27 21:05:20 +1300

    Fuck the state

--- app/services/generate_portfolio.rb
+++ app/services/generate_portfolio.rb
@@ -4,24 +4,12 @@ class GeneratePortfolio
   end
 
   def call
-    # get groups of order for each stock that user has
-    #@orders = Order.where(user: @user)
-
-    Order.where(user: @user).select(:stock_id).distinct
-    #stocks = Order.select(:stock).distinct
-
-    portfolio = []
-    Stock.includes(:orders).where(orders: {user: @user}).each do |stock|
+    Stock.includes(:orders).where(orders: {user: @user}).map do |stock|
       orders = stock.orders.fulfilled.where(user: @user)
 
-      market_price = stock.market_price
-      total_quantity = orders.sum(:quantity)
-
-      portfolio << Position.new(stock: stock,
-                                quantity: total_quantity,
-                                market_price: market_price)
+      Position.new(stock: stock,
+                   quantity: orders.sum(:quantity),
+                   market_price: stock.market_price)
     end
-
-    portfolio
   end
 end
