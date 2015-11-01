da3d61d6280291bcd1cb0eecc24e676f0e5f977c
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-11-01 18:50:18 +1300

    Fix portfolio position calculation

--- app/services/generate_portfolio.rb
+++ app/services/generate_portfolio.rb
@@ -5,17 +5,26 @@ class GeneratePortfolio
 
   def call
     Stock.for_user(@user).map do |stock|
-      orders = stock.orders.fulfilled.for_user(@user)
+      buy_orders = stock.buy_orders.fulfilled.for_user(@user)
+      sell_orders = stock.sell_orders.fulfilled.for_user(@user)
 
       Position.new(stock: stock,
-                   quantity: orders.sum(:quantity),
+                   quantity: sum_orders(buy_orders, sell_orders),
                    market_price: stock.market_price,
-                   cost: cost(orders)
+                   cost: sum_cost(buy_orders, sell_orders)
                   )
     end
   end
 
   private
+
+  def sum_orders(buy_orders, sell_orders)
+    buy_orders.sum(:quantity) - sell_orders.sum(:quantity)
+  end
+
+  def sum_cost(buy_orders, sell_orders)
+    cost(buy_orders) - cost(sell_orders)
+  end
 
   def cost(orders)
     orders.reduce(0) do |sum, order|
