539e6d181eea399706ccb212e82c26e857e5f686
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-11-10 11:00:09 +1300

    Test that MatchOrders is being called from job

--- spec/jobs/match_orders_job_spec.rb
+++ spec/jobs/match_orders_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe MatchOrdersJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:stock) { create(:stock) }
+
+  describe '#perform' do
+    it 'Calls Matchorders' do
+      match_orders_instance = instance_double('FulfillOrder')
+      allow(MatchOrders).to receive(:new).with(stock: stock).and_return(match_orders_instance)
+      expect(match_orders_instance).to receive(:call)
+
+      MatchOrdersJob.perform_now(stock: stock)
+    end
+  end
 end
